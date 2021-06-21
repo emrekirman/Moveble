7f69b8eeebc930f882ecef7c766c0f2793070a66
Emre Kirman <emrekirman10@gmail.com>
2021-06-22 02:23:34 +0300

    .

--- src/main/java/com/moveble/api/MeasurementUnitsController.java
+++ src/main/java/com/moveble/api/MeasurementUnitsController.java
@@ -3,8 +3,11 @@ package com.moveble.api;
 import com.moveble.entity.MeasurementUnit;
 import com.moveble.service.interfaces.IMeasurementUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/measurementUnits")
@@ -18,12 +21,21 @@ public class MeasurementUnitsController {
         this.measurementUnitService = measurementUnitService;
     }
 
-    @RequestMapping(method = RequestMethod.POST,value = "create")
+    @RequestMapping(method = RequestMethod.POST, value = "create")
     public ResponseEntity<MeasurementUnit> create(@RequestBody MeasurementUnit model) {
         try {
             return ResponseEntity.ok(measurementUnitService.create(model));
         } catch (Exception e) {
             throw e;
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getAll")
+    public ResponseEntity<List<MeasurementUnit>> getAll() {
+        try {
+            return ResponseEntity.ok(measurementUnitService.getAll());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
